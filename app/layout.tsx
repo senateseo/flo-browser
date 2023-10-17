e101da259ea9a57a4ebfb72b41cbac13fe2e2fbe
senateseo <swdanielseo@gmail.com>
2023-10-17 09:13:13 -0700

    Update favicon and Site description

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,7 @@ const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'FLO',
-  description: 'Create your own workFLO.',
+  description: 'Create your own FLO',
 }
 
 export default function RootLayout({
